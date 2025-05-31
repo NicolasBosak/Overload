08d19bac09032ed91a03549eed5e4df025602f5a
Nicolas <sebastian.casamen@udla.edu.ec>
2025-05-31 14:05:08 -0500

    Fix migrations for the new controllers.

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,41 +45,4 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function posts()
-    {
-        // One to Many
-        return $this->hasMany(Post::class);
-    }
-    public function likes()
-    {
-        // One to Many
-        return $this->hasMany(Like::class);
-    }
-    public function followers()
-    {
-        
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
-    }
-    //En el caso de arriba estamos tenemos que definir las llaves foraneas y la tabla de donde sacara la relacion ya que nos estamos saliendo un poco de las convenciones de Larave
-
-    // Comprobate if a user follow
-
-    public function following(User $user)
-    {
-        return $this->followers->contains($user->id);
-    }
-
-    public function follow()
-    {
-        
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
-    }
-    // Es exactamente igual al metodo "followers" solo que se cambia el roden de las columnas, antes era primer 'user_id' -> 'follower_id' ahora es 'follower_id' -> 'user_id'
-    
-    // Almacenar seguidores de un usuario
-
-
-
-    // Almancenar los que seguimos
 }
---
+++ database/seeders/CategoriasSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categorias')->insert([
+            'categoria' => 'Terror',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'Carreras',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'Acción',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'Aventura',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'Deportes',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'Third Person Shoter',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categorias')->insert([
+            'categoria' => 'RPG',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
